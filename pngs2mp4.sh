ea4761775026fa09f300981aedf798386e7ed4e3
Lukasz Gryglicki <lgryglicki@cncf.io>
2025-08-13 16:52:56 +0200

    Update ffmpeg video generator
    Signed-off-by: Lukasz Gryglicki <lgryglicki@cncf.io>

--- pngs2mp4.sh
+++ pngs2mp4.sh
@@ -9,7 +9,7 @@ fi
 prefix="$1"
 if [ -z "$LL" ]
 then
-  ffmpeg -y -framerate 24 -pattern_type glob -i "pngs/${prefix}_*.png" -c:v libx265 -preset slow -pix_fmt yuv444p12le -x265-params crf=2:tune=grain:aq-mode=3 -movflags +faststart "${prefix}.mp4"
+  ffmpeg -y -framerate 10 -pattern_type glob -i "pngs/${prefix}_*.png" -c:v libx265 -preset slow -pix_fmt yuv444p12le -x265-params crf=2:tune=grain:aq-mode=3 -movflags +faststart "${prefix}.mp4"
 else
-  ffmpeg -y -framerate 24 -pattern_type glob -i "pngs/${prefix}_*.png" -c:v libx265 -preset slow -pix_fmt yuv444p12le -x265-params lossless=1 -movflags +faststart "${prefix}.mp4"
+  ffmpeg -y -framerate 10 -pattern_type glob -i "pngs/${prefix}_*.png" -c:v libx265 -preset slow -pix_fmt yuv444p12le -x265-params lossless=1 -movflags +faststart "${prefix}.mp4"
 fi
