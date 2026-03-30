13cf428ba1cafa01c3c3fef1840c8d9cc6835266
Lukasz Gryglicki <lukaszgryglicki@o2.pl>
2026-03-30 14:19:59 +0000

    Re-run scenes with newest algo
    Signed-off-by: Lukasz Gryglicki <lukaszgryglicki@o2.pl>

--- run_all_scenes.sh
+++ run_all_scenes.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # FORCE_ESCAPE=1 SPP_ADJUST=1 ./run_all_scenes.sh
 rm gifs/*.gif
-for f in $(find ./scenes/ -iname "*.json")
+for f in $(find ./scenes/ -iname "*.json" ! -iname "ultimate_all_objects_trap.json")
 do
   echo "Processing scene: $f"
   ./photons4d "${f}"
